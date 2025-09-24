b9b7d04ca15222789dbc819feff6dd9eb11cc11b
Harisharan Mishra <131943689+HarisharanM@users.noreply.github.com>
2025-09-24 22:04:28 +0530

    Initial project setup complete

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,7 +1,7 @@
 import NextAuth from "next-auth"
 import Credentials from "next-auth/providers/credentials"
 import { prisma } from "@/lib/prisma"
-import bcrypt from "bcrypt"
+import bcrypt from "bcryptjs"
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
   pages: {
